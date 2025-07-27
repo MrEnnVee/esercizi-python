709b11c2c2513069970e69fa287f74123c35272d
MrEnnVee <nicola.vitale89@live.com>
2025-07-27 20:40:44 +0200

    esercizio 1

---
+++ maggiore_tra_2_numeri_con_uguaglianza.py
@@ -0,0 +1,15 @@
+# in questo esercizio trovo il maggiore tra due numeri con uguaglianza sfruttando strutture decisionali
+a=int(input("Inserisci il primo numero: "))
+b=int(input("Inserisci il secondo numero: "))
+if a>b:
+    print("Il maggiore è:", a)
+elif b>a:
+    print("Il maggiore è:", b)
+else:
+    print("I numeri sono uguali", a)
+
+
+
+# fine esercizio
+
+
