5c715386a7aa5fbf29b2cd262211eaee2d34f67a
MrEnnVee <nicola.vitale89@live.com>
2025-07-30 20:40:15 +0200

    gestione dell'operatore modulo con stuttura condizionale

---
+++ numero_pari_o_dispari.py
@@ -0,0 +1,10 @@
+# utilizzo dell' operatore modulo per verificare se un numero è pari o dispari
+
+print("NUMERO PARI O DISPARI")
+
+n=int(input("Inserisci un numero: "))
+
+if n%2==0:
+    print("Il numero", n, "è pari.")
+else:
+    print("Il numero", n, "è dispari.")
